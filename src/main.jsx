0528186deb16c362dd8f527565e8fe2091d8f7a7
BegimayS6900 <begimajsarmanalieva@gmail.com>
2025-09-08 11:48:06 +0600

    about

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './app/App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App />
-  </>,
+  </>
 )
---
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+import React from 'react'
+
+const AboutUsPage = () => {
+  return (
+    <div>
+        <div class="container bg-[#2563EB] text-white">
+          <h2 class="text-[56px] font-bold">About Speak Up</h2>
+          <p>SpeakUp is a ground-breaking app for learning  English , Turkish , France words and improving vocabulary for all speakers of English, from native speakers to beginner learners.It makes language learning more accessible to everyone around the globe.</p>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default AboutUsPage
